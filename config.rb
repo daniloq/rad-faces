23a072705d4c1e0cce4cb85682d95b5cf6b6c055
daniloquilaton <daniloquilaton@gmail.com>
2016-02-02 22:35:54 -0500

    Minify css

--- config.rb
+++ config.rb
@@ -58,7 +58,7 @@ set :build_dir, 'tmp'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
